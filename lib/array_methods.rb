fab4758f8ceae5fee7432abcff9ef92ce9c43cd4
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-02 19:36:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,12 +3,12 @@ def find_element_index(array, value_to_find)
     if array[count] == value_to_find
       return count
     end
-  nil
  end
+ nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+  
 end
 
 def find_min_value(array)
